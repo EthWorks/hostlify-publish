0eef582a48b84168a31fe5b3285180f9a31e9b5b
mateusz <mistrzumati@spoko.pl>
2022-02-23 15:10:29 +0100

    Add file getting

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('publish-dir');
-  console.log(nameToGreet);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const files = core.getInput('files')
+  console.log(files)
+  const id = core.getInput('id')
+  console.log(id)
+  const url = `${id}.truefipreview.io`
+  core.setOutput("url", url)
+  console.log("myurl " + url)
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log('files ', github.files)
+  console.log('input ', github.getInput)
+//   console.log(`The event payload: ${payload}`)
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
