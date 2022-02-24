3c6ce7cfc7d81487e5086e2e6acabb403c6474cb
mateusz <mistrzumati@spoko.pl>
2022-02-24 11:09:34 +0100

    Add uploiding files

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const fs = require('fs')
+const axios = require('axios')
+
+function addFilesToBody(mainPath, body, serverPath) {
+    fs.readdirSync(mainPath).forEach(fileOrFolderName => {
+        const currentLocalPath = `${mainPath}/${fileOrFolderName}`
+        const currentServerPath = `${serverPath}/${fileOrFolderName}`
+        if(fs.lstatSync(currentLocalPath).isDirectory()) {
+            body = addFilesToBody(currentLocalPath, body, currentServerPath)
+        }
+        else {
+            const fileData = fs.readFileSync(currentLocalPath)
+            const fileObject = {
+                name: fileOrFolderName,
+                data: fileData.toString()
+            }
+            body[currentServerPath] = fileObject
+        }
+    })
+    return body
+}
+
+function sendFiles(mainPath, id) {
+    const url = `http://lvh.me/upload/${id}`
+    let body = {}
+    body = addFilesToBody(mainPath, body, '.')
+    console.log(body)
+    axios.post(url, body, (err) => {
+        if(err) {
+            console.log(err)
+        }
+    })
+}
 
 try {
   const files = core.getInput('files')
-  console.log(files)
   const id = core.getInput('id')
-  console.log(`id ${id}`)
-  const url = `${id}.truefipreview.io`
-  core.setOutput("url", url)
-  console.log("myurl " + url)
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log('files ', core.files)
-  console.log('input ', github.getInput)
-//   console.log(`The event payload: ${payload}`)
+  const serverUrl = core.getInput('server-url')
+  const url = `${id}.${serverUrl}`
+  sendFiles(files, id)
+  core.setOutput('url', url)
 } catch (error) {
-  core.setFailed(error.message)
+    console.log(error)
+    core.setFailed(error.message)
 }
