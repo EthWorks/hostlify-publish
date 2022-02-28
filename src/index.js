ada49fe4ebd9f247226b892b15dded1c80a5f3a4
mateusz <mistrzumati@spoko.pl>
2022-02-28 11:00:25 +0100

    UV

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ async function run() {
     try {
     const { files, serverUrl } = getInputs()
     const id = github.sha
-    console.log(github.commits)
+    console.log("git: " + github)
     const previewUrl = `${id}.${serverUrl}`
     sendFiles(files, serverUrl, id)
     core.setOutput('url', previewUrl)
