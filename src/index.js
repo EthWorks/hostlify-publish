5eba95479a1a3cf9518ae1d42925097b04697065
mateusz <mistrzumati@spoko.pl>
2022-02-28 10:45:17 +0100

    UV

--- src/index.js
+++ src/index.js
@@ -46,7 +46,8 @@ function sendFiles(mainPath, url, id) {
 async function addComment(commentContent) {
     const { owner, repo, pullNumber, accessToken } = getInputs()
     const octokit = new Octokit({ auth: accessToken})
-    const urlHtml = `<a href="http://${commentContent}">${commentContent + " " + github}</a>`
+    console.log(github)
+    const urlHtml = `<a href="http://${commentContent}">${commentContent}</a>`
     await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
         owner,
         repo,
